40c9d5ace389b786277cdecf480146f219e966ce
Vineet Kumar Verma <vineetkverma1947@gmail.com>
2022-10-15 15:16:45 +0530

    heroku

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use('/api/user', userRoute);
 app.use('/api/admin', adminRoute);
 app.use('/api/doctor', doctorRoute);
 
+const port = process.env.PORT || 5000;
+
 if (process.env.NODE_ENV === 'production') {
   app.use('/', express.static('client/build'));
 
@@ -19,9 +21,8 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
   });
 }
-const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Node Express Server Started at ${port}!`));
 
 // heroku files
